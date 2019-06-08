71fafbda9c6aaf6450e1d3b12b07e5548a690488
XEonAX <sv.xeon@gmail.com>
2019-06-08 17:01:05 +0530

    Disable Poco Libs for cepheus and perseus

--- src/ANXCameraUnity/common/service.sh
+++ src/ANXCameraUnity/common/service.sh
@@ -1,10 +1,6 @@
-# This script will be executed in late_start service mode
-# More info in the main Magisk thread
+# # This script will be executed in late_start service mode
+# # More info in the main Magisk thread
 
-if [ device_check "cepheus" ] || [ device_check "perseus" ];
-	then 
-		rm -rf $MODPATH/system/lib/
-		rm -rf $MODPATH/system/lib64/
-fi
-
+sleep 6;
 killall -9 android.hardware.camera.provider@2.4-service
+ui_print "anx demands camserver restart"
---
+++ src/ANXCameraUnity/common/unity_install.sh
@@ -0,0 +1,29 @@
+
+ROPRODEV=$(getprop ro.product.device)
+
+ui_print "Your device is recognized as $ROPRODEV" 
+ui_print "It should be one of below:" 
+ui_print "  beryllium"
+ui_print "  dipper"
+ui_print "  polaris"
+ui_print "  perseus"
+ui_print "  cepheus"
+ui_print "  lavender"
+ui_print "  violet"
+ui_print "  whyred"
+ui_print ""
+ui_print "In decreasing order of support"
+ui_print ""
+ui_print ""
+
+if device_check "cepheus"; then 
+	rm -rf $TMPDIR/system/lib/
+	rm -rf $TMPDIR/system/lib64/
+	ui_print "cephy was here, it hates poco libs"
+fi
+
+if device_check "perseus"; then 
+	rm -rf $TMPDIR/system/lib/
+	rm -rf $TMPDIR/system/lib64/
+	ui_print "persy was here, it hates poco libs"
+fi
