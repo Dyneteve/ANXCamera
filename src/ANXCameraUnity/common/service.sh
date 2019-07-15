1df060a607187e30334b57aecdc196e47ce77d3d
Dyneteve <sound0020@gmail.com>
2019-07-15 19:36:44 +0300

    Don't restart camera provider on violet

--- src/ANXCameraUnity/common/service.sh
+++ src/ANXCameraUnity/common/service.sh
@@ -1,6 +1,12 @@
 # # This script will be executed in late_start service mode
 # # More info in the main Magisk thread
 
-sleep 4;
-killall -9 android.hardware.camera.provider@2.4-service
-ui_print "anx demands camserver sacrifice and rebirth"
+if device_check "violet"; then 
+  ui_print "violety condemns camserver sacrifice"
+elif [ -f /sdcard/.anxnosacrifice ]; then
+  ui_print "user shows mercy on camserver sacrifice"
+else
+  sleep 4
+  killall -9 android.hardware.camera.provider@2.4-service
+  ui_print "anx demands camserver sacrifice and rebirth"
+fi
--- src/ANXCameraUnity/common/unity_install.sh
+++ src/ANXCameraUnity/common/unity_install.sh
@@ -4,12 +4,12 @@ ROPRODEV=$(getprop ro.product.device)
 ui_print "Your device is recognized as $ROPRODEV" 
 ui_print "It should be one of below:" 
 ui_print "  beryllium"
+ui_print "  violet"
 ui_print "  dipper"
 ui_print "  polaris"
 ui_print "  perseus"
 ui_print "  cepheus"
 ui_print "  lavender"
-ui_print "  violet"
 ui_print "  whyred"
 ui_print "  equuleus"
 ui_print "  raphael"
@@ -27,35 +27,26 @@ ui_print ""
 ui_print ""
 
 if device_check "cepheus"; then 
-	rm -rf $TMPDIR/system/lib/
-	rm -rf $TMPDIR/system/lib64/
-	ui_print "cephy was here, it hates poco libs"
+  rm -rf $TMPDIR/system/lib/
+  rm -rf $TMPDIR/system/lib64/
+  ui_print "cephy was here, it hates poco libs"
+elif device_check "perseus"; then 
+  rm -rf $TMPDIR/system/lib/
+  rm -rf $TMPDIR/system/lib64/
+  ui_print "persy was here, it hates poco libs"
+elif device_check "violet"; then 
+  rm -rf $TMPDIR/system/lib/
+  rm -rf $TMPDIR/system/lib64/
+  ui_print "violety was here, it hates poco libs, it loves someone else"
+elif device_check "whyred"; then 
+  rm -rf $TMPDIR/system/lib/
+  rm -rf $TMPDIR/system/lib64/
+  ui_print "whyredy was here, it hates poco libs, it loves someone else"
+elif [ -f /sdcard/.anxnolib ]; then
+  rm -rf $TMPDIR/system/lib/
+  rm -rf $TMPDIR/system/lib64/
+  ui_print "User hates poco libs, they love someone else"
 fi
-
-if device_check "perseus"; then 
-	rm -rf $TMPDIR/system/lib/
-	rm -rf $TMPDIR/system/lib64/
-	ui_print "persy was here, it hates poco libs"
-fi
-
-if device_check "violet"; then 
-	rm -rf $TMPDIR/system/lib/
-	rm -rf $TMPDIR/system/lib64/
-	ui_print "violety was here, it hates poco libs, it loves someone else"
-fi
-
-if device_check "whyred"; then 
-	rm -rf $TMPDIR/system/lib/
-	rm -rf $TMPDIR/system/lib64/
-	ui_print "whyredy was here, it hates poco libs, it loves someone else"
-fi
-
-if [ -f /sdcard/.anxnolib ]; then
-	rm -rf $TMPDIR/system/lib/
-	rm -rf $TMPDIR/system/lib64/
-	ui_print "User hates poco libs, they love someone else"
-fi
-
 
 MNAME=$(grep_prop name $TMPDIR/module.prop)
 MDEV=$(grep_prop author $TMPDIR/module.prop)
